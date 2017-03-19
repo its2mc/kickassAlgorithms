88a3bf24dad3ba6e91087dc1ba0a1e8cddbf25de
its2mc <its2uraps@gmail.com>
2017-03-19 13:04:34 +0900

    added extra sauces

--- utilityFunctions.js
+++ utilityFunctions.js
@@ -11,25 +11,24 @@
  * **warning** 
  * 
  * This function iterates through an array of objects and aggregates a chosen field from the objects
+ * @param {Array} arr Array containing 
  */
-function _uniqByAggregate(arr) {
+function _uniqByAggregate(arr, uniqField, aggrField) {
     for (let i = 0, j = 1; i < arr.length - 1; ++j, ((!(j < arr.length)) ? (++i, j = i + 1) : null))
-        (arr[i].keyword === arr[j].keyword) ? (arr[i].freq += (arr.splice(j, 1))[0].freq, j--) : null;
+        (arr[i][uniqField] === arr[j][uniqField]) ? (arr[i][aggrField] += (arr.splice(j, 1))[0][aggrField], j--) : null;
     return arr;
 }
 
 
 /**
  * **warning** 
- * This algorithm does not mutate the input algorithm, bt it is one line longer than the previous one
- * and it is slightly slower
+ * This algorithm does not mutate the input algorithm, bt it is slightly slower
  * **warning** 
  * 
  * This function iterates through an array of objects and aggregates a chosen field from the objects
  */
-function uniqByAggregate(arr) {
-    let res = arr.splice();
+function uniqByAggregate(arr, uniqField, aggrField, res = arr.splice()) {
     for (let i = 0, j = 1; i < res.length - 1; ++j, ((!(j < res.length)) ? (++i, j = i + 1) : null))
-        (res[i].keyword === res[j].keyword) ? (res[i].freq += (res.splice(j, 1))[0].freq, j--) : null;
+        (res[i][uniqField] === res[j][uniqField]) ? (res[i][aggrField] += (res.splice(j, 1))[0][aggrField], j--) : null;
     return res;
 }
